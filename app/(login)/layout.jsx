07fe420eabe923078caff0b5a52905540fc4dbc7
Andrea De la cruz <aj.dlacruz.7@gmail.com>
2024-07-10 20:48:40 -0500

    Inicial commit

---
+++ app/(login)/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const layout = ({children}) => {
+  return (
+    <main className=' items-center justify-center h-screen '>
+{children}
+    </main>
+  )
+}
+
+export default layout
---
+++ app/(login)/page.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+const page = () => {
+  return (
+  <section className='border  max-w-screen-xl text-center lg:px-14'>
+    <section className="bg-center bg-no-repeat bg-cover bg-[url('https://i.pinimg.com/564x/cc/9c/04/cc9c0456047d7c26bc84117d64369a1a.jpg')] bg-gray-600 bg-blend-multiply ">
+      <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56 ">
+      <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl ">Restaurante </h1>
+      <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl ">Sabores y Aromas</h1>
+      <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Siente el sabor, huele la diferencia</p>
+      <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+         <Link href="/sign-in" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+             Sign in
+             <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+             </svg>
+            </Link>
+         <Link href="/sign-up" className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                Sign Up
+         </Link>  
+        </div>
+        <p className=" my-2 mb-8 text-xs font-normal text-gray-400 lg:text-xs sm:px-16 lg:px-48">Logueate para disfrutar de nuestra experiencia.</p>
+      </div>
+    </section>
+
+  </section>
+  )
+}
+
+export default page
